38d3a26174f3d583e707656b9c35a14e597bb4fd
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-10-05 01:09:53 +0530

    Add public-samples/pollen8-platform-268ixb/src/backend/.eslintrc.js

---
+++ src/backend/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // Custom rule configurations can be added here
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-call': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  env: {
+    node: true,
+    es2020: true,
+  },
+  ignorePatterns: ['dist', 'node_modules', '**/*.js'],
+};
